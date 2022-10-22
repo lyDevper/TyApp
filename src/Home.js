c76e2faaa0aa47bfad5da6e468ca2d26b4650400
noeinatha <92200039+noeinatha@users.noreply.github.com>
2022-10-23 00:11:56 +0700

    Edit Package.json

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,11 @@
-import { useState } from 'react';
+import { useState } from "react";
 //import './Login.css';
 
 function Home() {
-    const [username, setUserame] = useState('');
-    const [password, setPassword] = useState('');
+  const [username, setUserame] = useState("");
+  const [password, setPassword] = useState("");
 
-  return (
-    <div className='backGround'>
-        
-    </div>
-  );
+  return <div className="backGround"></div>;
 }
-
-
 
 export default Home;
