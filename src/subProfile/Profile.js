5f631e6b5a904e6bbb2d721ff82cdccaa611c9e9
Fa2ijai <112095289+Fa2ijai@users.noreply.github.com>
2022-10-26 18:04:45 +0700

    แก้cssProfile

--- src/subProfile/Profile.js
+++ src/subProfile/Profile.js
@@ -11,29 +11,31 @@ export default function Profile() {
 
   return (
     <div className={classes.backGround}>
-        <div className={classes.contentBlock}>
-            <div>
-            <lebel className={classes.head1}>โปรไฟล์</lebel>
-            <hr className={classes.line}></hr>
-            {/* <input type="file" className={classes.profileBtn} /> */}
-            <button className={classes.profileBtn}><FiCamera size={50}/></button>
+        <div className={classes.contentbg}>
+            <div className={classes.contentBlock}>
+                <div>
+                <lebel className={classes.head1}>โปรไฟล์</lebel>
+                <hr className={classes.line}></hr>
+                {/* <input type="file" className={classes.profileBtn} /> */}
+                <button className={classes.profileBtn}><FiCamera size={50}/></button>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <label className={classes.lbl1}>Username </label>
+                <div className={classes.inp1} style={{display: "inline-block"}}>
+                    <input type="text" className={classes.realinp}
+                    value={newusername} onChange={(e) => setNewusername(e.target.value)} />
+                    <lebel className={classes.inpicon}><HiOutlinePencil/></lebel>
+                </div>
+                <br/>
+                <label className={classes.lbl2}>Email </label>
+                <div className={classes.inp2} style={{display: "inline-block"}}>
+                    {"email"}
+                </div>
+                    <button className={classes.btnSave} onClick={() => save(newusername)}>บันทึกข้อมูล</button>
             </div>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <label className={classes.lbl1}>Username </label>
-            <div className={classes.inp1} style={{display: "inline-block"}}>
-                <input type="text" className={classes.realinp}
-                value={newusername} onChange={(e) => setNewusername(e.target.value)} />
-                <lebel className={classes.inpicon}><HiOutlinePencil/></lebel>
-            </div>
-            <br/>
-            <label className={classes.lbl2}>Email </label>
-            <div className={classes.inp2} style={{display: "inline-block"}}>
-                {"email"}
-            </div>
-                <button className={classes.btnSave} onClick={() => save(newusername)}>บันทึกข้อมูล</button>
         </div>
         <div className={classes.backBanner}>
             <button className={classes.btnHome} onClick={() => goHome()}><AiFillHome size={40}/></button>
