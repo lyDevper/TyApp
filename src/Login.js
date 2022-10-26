137782638f7e95c325636fa808c2107ea6e1973c
lyDevper <luckydekdee@gmail.com>
2022-10-26 18:19:07 +0700

    แก้หน้า Login

--- src/Login.js
+++ src/Login.js
@@ -9,21 +9,30 @@ function Login() {
   return (
     <div className={classes.backGround}>
         <div className={classes.backBanner}>
+            <div className={classes.descriptTxtDiv}>
+                <h1 className={classes.appTitle}>ชื่อแอปโตโต๊โต</h1>
+                <h1 className={classes.appTitle}>ตามหาเพื่อนรวมทาง</h1>
+                <p className={classes.descriptTxt}>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores minima ullam illo a harum commodi nesciunt vel at. Alias unde iusto sunt dicta maxime accusamus illum autem ex voluptates dolorem.
+                </p>
+            </div>
         </div>
         <div className={classes.signInBox}>
             <h2 className={classes.head1}>เข้าสู่ระบบ</h2>
 
-            <label className={classes.lbl1}>Email :</label>
-            <input type="Email" className={classes.inp1}
-            value={email} onChange={(e) => setEmail(e.target.value)} />
+            <div className={classes.formDiv}>
+                <label className={classes.lbl1}>Email :</label>
+                <input type="Email" className={classes.inp1}
+                value={email} onChange={(e) => setEmail(e.target.value)} />
 
-            <label className={classes.lbl1}>Password :</label>
-            <input type="Password" className={classes.inp1}
-            value={password} onChange={(e) => setPassword(e.target.value)} />
+                <label className={classes.lbl1}>Password :</label>
+                <input type="Password" className={classes.inp1}
+                value={password} onChange={(e) => setPassword(e.target.value)} />
+            </div>            
 
             <div className={classes.buttonDiv}>
-                <button className={classes.btn1} onClick={() => goSignUp()}>สมัครสมาชิก</button>
-                <button className={classes.btn1} onClick={() => goSignIn(email, password)}>เข้าสู่ระบบ</button>
+                <button className={classes.btn1+' '+classes.pushedBtn} onClick={() => goSignUp()}>สมัครสมาชิก</button>
+                <button className={classes.btn1+' '+classes.pushedBtn} onClick={() => goSignIn(email, password)}>เข้าสู่ระบบ</button>
             </div>
         </div>
     </div>
